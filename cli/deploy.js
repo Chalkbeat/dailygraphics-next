58cb17d56cb0b85cf509064ed28ed0541b2961c3
Thomas Wilburn <twilburn@chalkbeat.org>
2024-11-07 09:40:45 -0600

    Add a --filter for CLI deployment

--- cli/deploy.js
+++ cli/deploy.js
@@ -2,10 +2,11 @@ var deploy = require("../lib/deployGraphic");
 var { completeSlug } = require("./util");
 
 module.exports = async function(config, argv, slugs) {
+  var filter = argv.filter?.split(",");
   for (var slug of slugs) {
     try {
       slug = await completeSlug(config.root, slug);
-      await deploy(config, slug);
+      await deploy(config, slug, filter);
     } catch (err) {
       console.error(err);
     }
